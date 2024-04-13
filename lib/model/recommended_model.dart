352889ea3d07958053c1dc20049a206dc84a59b3
BahTea <166146303+bahtibegum@users.noreply.github.com>
2024-04-13 14:14:25 +0200

    Add files via upload

---
+++ lib/model/recommended_model.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class RecommendedModel{
+  Icon? icon;
+  String? name;
+  String? type;
+  String? salary;
+
+  RecommendedModel({this.name, this.icon, this.salary, this.type});
+
+  static List<RecommendedModel> recommended = [
+
+    RecommendedModel(
+        icon: const Icon(Icons.mobile_friendly,
+          color: Colors.blue,
+          size: 35,
+        ),
+        name: 'App Designer',
+        type: 'Full Time',
+        salary: '\$180'
+    ),
+    RecommendedModel(
+        icon: const Icon(Icons.desktop_mac,
+          color: Colors.blue,
+          size: 35,
+        ),
+        name: 'Desktop Developer',
+        type: 'Full Time',
+        salary: '\$80'
+    ),
+
+    RecommendedModel(
+        icon: const Icon(Icons.web,
+          color: Colors.blue,
+          size: 35,
+        ),
+        name: 'Web Developer',
+        type: 'Part Time',
+        salary: '\$30'
+    ),
+
+
+
+  ];
+}
